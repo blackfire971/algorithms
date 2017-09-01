dae5a273363ca7adecd858d49580af94d6613958
saml <samli6572@gmail.com>
2017-09-01 15:27:06 -0500

    update

--- src/com/algorithms/MergingSort.java
+++ src/com/algorithms/MergingSort.java
@@ -9,4 +9,31 @@ package com.algorithms;
  * Divide and conquer method used for algorithm implementation
  */
 public class MergingSort {
+    public static void sort(int[] a, int p, int q, int r) {
+        int n1 = q - p + 1;
+        int n2 = r - q;
+
+        int[] la = new int[n1];
+        int[] ra = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            la[i] = a[p + i];
+        }
+        for (int i = 0; i < n2; i++) {
+            ra[i] = a[q + i];
+        }
+
+        int lc = 0;
+        int rc = 0;
+
+        for (int i = 0; i < r - p + 1; i++) {
+            if (la[lc] <= ra[rc]) {
+                a[i] = la[lc];
+                lc++;
+            } else {
+                a[i] = ra[rc];
+                rc++;
+            }
+        }
+    }
 }
