4727c3fd1fdae5b24f770a7fc626d835f9e0d379
saml <samli6572@gmail.com>
2017-09-07 16:44:44 -0500

    update

---
+++ src/com/leetcode/easy/PalindromeNumberSolution.java
@@ -0,0 +1,29 @@
+package com.leetcode.easy;
+
+/**
+ * Created by saml on 9/7/2017.
+ * Determine whether an integer is a palindrome. Do this without extra space.
+ */
+public class PalindromeNumberSolution {
+
+        public boolean isPalindrome(int x) {
+            String s = String.valueOf(x);
+            if (s.startsWith("-")) {
+                return false;
+            }
+            char[] chars = s.toCharArray();
+
+            for (int i = 0; i < chars.length / 2; i++) {
+                if (chars[i] != chars[chars.length - 1 - i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+    public static void main(String[] args) {
+        PalindromeNumberSolution palindromeNumberSolution = new PalindromeNumberSolution();
+        System.out.println(palindromeNumberSolution.isPalindrome(2147447412));
+    }
+}
