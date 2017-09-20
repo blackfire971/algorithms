04b89d8edf26f351d174cc9978bf9630d594f403
saml <samli6572@gmail.com>
2017-09-20 16:56:18 -0500

    update

---
+++ src/com/leetcode/easy/SearchInsertPositionSolution.java
@@ -0,0 +1,46 @@
+package com.leetcode.easy;
+
+/**
+ * Created by saml on 9/15/2017.
+ * Given a sorted array and a target value, return the index if the target is found. If not, return the index where
+ * it would be if it were inserted in order.
+ * <p>
+ * You may assume no duplicates in the array.
+ * <p>
+ * Here are few examples.
+ * [1,3,5,6], 5 → 2
+ * [1,3,5,6], 2 → 1
+ * [1,3,5,6], 7 → 4
+ * [1,3,5,6], 0 → 0
+ */
+public class SearchInsertPositionSolution {
+
+    public static void main(String[] args) {
+        SearchInsertPositionSolution searchInsertPositionSolution = new SearchInsertPositionSolution();
+        int[] a = {1, 3, 5, 6};
+        System.out.println(searchInsertPositionSolution.searchInsert(a, 2));
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int start = 0;
+        int end = nums.length - 1;
+        int result = 0;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (target == nums[mid] || start == end) {
+                result = end;
+                break;
+            } else if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            }
+        }
+        return result == 0 ? result : result + 1;
+    }
+}
