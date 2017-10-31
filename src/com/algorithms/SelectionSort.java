5ec326257613626bca64346ec92261ce6f7bbf30
saml <samli6572@gmail.com>
2017-10-31 09:54:51 -0500

    update

---
+++ src/com/algorithms/SelectionSort.java
@@ -0,0 +1,38 @@
+package com.algorithms;
+
+/**
+ * Created by saml on 10/31/2017.
+ */
+public class SelectionSort extends SortTemplate {
+
+    public static void sort(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < a.length; j++) {
+                count++;
+                if (less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            exch(a, i, min);
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] is = new Integer[8];
+        is[0] = 10;
+        is[1] = 7;
+        is[2] = 6;
+        is[3] = 5;
+        is[4] = 2;
+        is[5] = 1;
+        is[6] = 11;
+        is[7] = 12;
+
+        Comparable[] a = is;
+        sort(a);
+        assert isSorted(a);
+        show(a);
+        System.out.println("complexity: " + count);
+    }
+}
