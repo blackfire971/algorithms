d4233a5799230c0600e1eaa239275dc5e9518ae1
blackfire971 <samli6572@gmail.com>
2018-03-20 23:46:43 -0500

    update

---
+++ src/com/algorithms4/sort/test2/Quick3Way.java
@@ -0,0 +1,45 @@
+package com.algorithms4.sort.test2;
+
+/**
+ * Created by Sam Li on 3/20/2018.
+ */
+public class Quick3Way {
+
+    public static void sort(int[] arr) {
+        sort(arr, 0, arr.length - 1);
+    }
+
+    private static void sort(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int pivot = arr[(high + low) / 2];
+        int i = low, j = high, cursor = low;
+        while (cursor <= j) {
+            if (arr[cursor] < pivot) {
+                exchange(arr, cursor++, i++);
+            } else if (arr[cursor] > pivot) {
+                exchange(arr, cursor, j--);
+            } else {
+                cursor++;
+            }
+        }
+        sort(arr, low, i - 1);
+        sort(arr, j + 1, high);
+    }
+
+
+    public static void exchange(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {2, 3, 6, 6, 3, 2, 5, 7, 89, 6, 4, 2, 1, 2, 56, 78, 9};
+        sort(array);
+        for (int item:array) {
+            System.out.print(item+",");
+        }
+    }
+}
