4fcc0ca5b7114032051d5590eb3df75ee12fdf0a
saml <samli6572@gmail.com>
2018-03-23 16:40:26 -0500

    update

---
+++ src/com/algorithms4/sort/MaxPQ.java
@@ -0,0 +1,40 @@
+package com.algorithms4.sort;
+
+/**
+ * Created by saml on 3/23/2018.
+ */
+public class MaxPQ {
+    public MaxPQ() {
+
+    }
+    public MaxPQ(int size) {
+
+    }
+
+    public MaxPQ(int[] arr) {
+
+    }
+
+    public void insert(int v) {
+
+    }
+
+    public int getMax() {
+
+        return 0;
+    }
+
+    public void deleteMax() {
+
+    }
+
+    public boolean isEmpty() {
+
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+}
